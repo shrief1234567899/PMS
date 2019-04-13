c6c67ac2f7f72b80fe8401091355a342ad268e21
sherif mohamed <sherif@cyberaccounting.ae>
2019-04-13 19:10:06 +0200

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pms.Models;
+
+namespace pms.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult CustomerHome()
+        {
+            return View("~/Views/Customer/Index.cshtml");
+        }
+    }
+}
